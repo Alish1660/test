56424c5732b71f832eced0c5c87ef444daaa9ce3
Alish1660 <git>
2024-06-22 17:03:43 +0500

    dsds

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      { find: "@", replacement: "/src/*" },
-      { find: "@routes", replacement: "/src/router/routes.jsx" },
-      { find: "@pages", replacement: "/src/pages/index.jsx" },
-      { find: "@service", replacement: "/src/service/index.js" },
-    ],
-  },
+  resolve: {},
 });
